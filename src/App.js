bcd6706313f0f7a935ceb44d0581e2742b5ee35a
samurai98 <kirito1498@gmail.com>
2020-05-10 22:22:24 +0300

    feat: add dispatch

--- src/App.js
+++ src/App.js
@@ -20,13 +20,11 @@ const App = (props) => {
             <div className={"app-wrapper-content"}>
                 <Route path='/profile' render={() => <Profile
                     profilePage={props.state.profilePage}
-                    addPost={props.addPost}
-                    updateNewPostText={props.updateNewPostText}/>}/>
+                    dispatch={props.dispatch}/>}/>
                 <Route path='/dialogs'
                        render={() => <Dialogs
                            state={props.state.dialogsPage}
-                           addMessage={props.addMessage}
-                           updateNewMessageText={props.updateNewMessageText}/>}/>
+                           dispatch={props.dispatch}/>}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/music' render={() => <Music/>}/>
                 <Route path='/settings' render={() => <Settings/>}/>
