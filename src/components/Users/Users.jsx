66ce75e4a5cbde999c0ce29349f92673ed0a50df
samurai98 <kirito1498@gmail.com>
2020-06-07 18:23:55 +0300

    refactor: add componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,13 +4,13 @@ import * as axios from 'axios';
 import userPhoto from '../../../src/assets/images/user.png'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
+
+    componentDidMount() {
         axios.get("https://social-network.samuraijs.com/api/1.0/users")
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
-    };
+    }
 
     render() {
         return <div>
