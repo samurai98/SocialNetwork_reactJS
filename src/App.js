56a0d7e044dc222305f8bc39873a99d64e74067e
samurai98 <kirito1498@gmail.com>
2020-06-15 23:14:48 +0300

    feat: add cookie, login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header'
 import Navbar from "./components/Navbar/Navbar";
 import {Route} from "react-router-dom";
 import News from "./components/News/News";
@@ -10,12 +9,13 @@ import PropTypes from "prop-types"
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = (props) => {
 
     return (
         <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer/>
             <Navbar state={props.state.sidebar}/>
             <div className={"app-wrapper-content"}>
                 <Route path='/profile/:userId?' render={() =>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
-        <img src='https://www.stickpng.com/assets/images/5a4fbafbda2b4f099b95da05.png'/>
+        <img alt='logo' src='https://www.stickpng.com/assets/images/5a4fbafbda2b4f099b95da05.png'/>
+
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
     </header>
-}
+};
 
 export default Header;
