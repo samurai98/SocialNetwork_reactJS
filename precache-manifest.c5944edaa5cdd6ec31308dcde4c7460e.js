6901a49471915c2b3918a2f99798c977dec6626f
samurai98 <kirito1498@gmail.com>
2020-08-05 13:51:38 +0300

    Updates

---
+++ precache-manifest.c5944edaa5cdd6ec31308dcde4c7460e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cebf2cca98b28bd96dfe2326930a846",
+    "url": "/SocialNetwork_reactJS/index.html"
+  },
+  {
+    "revision": "5020eae0224aec9df35d",
+    "url": "/SocialNetwork_reactJS/static/css/3.a9ce8d41.chunk.css"
+  },
+  {
+    "revision": "fdeea8615ca2a914e8e4",
+    "url": "/SocialNetwork_reactJS/static/css/4.e16f2ab7.chunk.css"
+  },
+  {
+    "revision": "762b21271fc182986f52",
+    "url": "/SocialNetwork_reactJS/static/css/main.cb7c523b.chunk.css"
+  },
+  {
+    "revision": "8da098ac454f0c2f55ec",
+    "url": "/SocialNetwork_reactJS/static/js/2.65e7cd44.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork_reactJS/static/js/2.65e7cd44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5020eae0224aec9df35d",
+    "url": "/SocialNetwork_reactJS/static/js/3.b6e5d8fa.chunk.js"
+  },
+  {
+    "revision": "fdeea8615ca2a914e8e4",
+    "url": "/SocialNetwork_reactJS/static/js/4.19da8ec2.chunk.js"
+  },
+  {
+    "revision": "762b21271fc182986f52",
+    "url": "/SocialNetwork_reactJS/static/js/main.c9afa910.chunk.js"
+  },
+  {
+    "revision": "b891b2a8a7e8b69cdd2c",
+    "url": "/SocialNetwork_reactJS/static/js/runtime-main.367fb823.js"
+  },
+  {
+    "revision": "461855b29ae2060f319f225529145f7c",
+    "url": "/SocialNetwork_reactJS/static/media/preloader.461855b2.gif"
+  },
+  {
+    "revision": "2c6e94aaf20d66610132b533ae100324",
+    "url": "/SocialNetwork_reactJS/static/media/user.2c6e94aa.png"
+  }
+]);
