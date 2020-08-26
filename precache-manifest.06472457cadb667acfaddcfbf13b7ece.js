6d7e2abe4d3e20d74b928f6cab755609acf7cc43
samurai98 <kirito1498@gmail.com>
2020-08-26 19:58:29 +0300

    Updates

---
+++ precache-manifest.06472457cadb667acfaddcfbf13b7ece.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5d3af850662e11fcc68c808d87bfa4",
+    "url": "/SocialNetwork_reactJS/index.html"
+  },
+  {
+    "revision": "0b3c4e75a938ec98b8c5",
+    "url": "/SocialNetwork_reactJS/static/css/3.c482f86b.chunk.css"
+  },
+  {
+    "revision": "d7fe346c7d37317c915d",
+    "url": "/SocialNetwork_reactJS/static/css/4.e16f2ab7.chunk.css"
+  },
+  {
+    "revision": "4c3d89427cfbdb8b0854",
+    "url": "/SocialNetwork_reactJS/static/css/main.654bf304.chunk.css"
+  },
+  {
+    "revision": "76aee6fbf9f3b55ef62f",
+    "url": "/SocialNetwork_reactJS/static/js/2.3c7e758c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork_reactJS/static/js/2.3c7e758c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3c4e75a938ec98b8c5",
+    "url": "/SocialNetwork_reactJS/static/js/3.3ee50503.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/SocialNetwork_reactJS/static/js/3.3ee50503.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7fe346c7d37317c915d",
+    "url": "/SocialNetwork_reactJS/static/js/4.c44e7c80.chunk.js"
+  },
+  {
+    "revision": "4c3d89427cfbdb8b0854",
+    "url": "/SocialNetwork_reactJS/static/js/main.fb8c0330.chunk.js"
+  },
+  {
+    "revision": "7e5e2aded83f9f706d81",
+    "url": "/SocialNetwork_reactJS/static/js/runtime-main.a7cf5593.js"
+  },
+  {
+    "revision": "707eca06b8af9f531175ca947a7b3c80",
+    "url": "/SocialNetwork_reactJS/static/media/dev.707eca06.png"
+  },
+  {
+    "revision": "a7f4f3bbf88de4392a5cca5223d9797e",
+    "url": "/SocialNetwork_reactJS/static/media/dialogs.a7f4f3bb.svg"
+  },
+  {
+    "revision": "b7d39a2277269c0ad8ae6de6e1234d72",
+    "url": "/SocialNetwork_reactJS/static/media/home.b7d39a22.svg"
+  },
+  {
+    "revision": "17f9b74b78857a02da753f5201cd7fd2",
+    "url": "/SocialNetwork_reactJS/static/media/logo.17f9b74b.png"
+  },
+  {
+    "revision": "c640764b78ea67555710c26df3c87192",
+    "url": "/SocialNetwork_reactJS/static/media/music.c640764b.svg"
+  },
+  {
+    "revision": "7d2f6fd99e7e9454e4d0f222318843bc",
+    "url": "/SocialNetwork_reactJS/static/media/news.7d2f6fd9.svg"
+  },
+  {
+    "revision": "461855b29ae2060f319f225529145f7c",
+    "url": "/SocialNetwork_reactJS/static/media/preloader.461855b2.gif"
+  },
+  {
+    "revision": "c9bff2d9c3360a897c675ce1bddde327",
+    "url": "/SocialNetwork_reactJS/static/media/settings.c9bff2d9.svg"
+  },
+  {
+    "revision": "2c6e94aaf20d66610132b533ae100324",
+    "url": "/SocialNetwork_reactJS/static/media/user.2c6e94aa.png"
+  },
+  {
+    "revision": "8c8f2cf984eeb31715a3b633e65be64f",
+    "url": "/SocialNetwork_reactJS/static/media/users.8c8f2cf9.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SocialNetwork_reactJS/precache-manifest.169301da429bdeb2d8a02a6ac00b3efd.js"
+  "/SocialNetwork_reactJS/precache-manifest.06472457cadb667acfaddcfbf13b7ece.js"
 );
 
 self.addEventListener('message', (event) => {
